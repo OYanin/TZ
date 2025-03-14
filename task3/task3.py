13f3c12d3927da0e70c8883de53d7a19b82b21fd
Oleg Yanin <textarea@mail.ru>
2025-03-14 13:52:18 +0500

    add

---
+++ task3/task3.py
@@ -0,0 +1,17 @@
+import os.path
+import os
+from sys import argv
+import json
+
+#f_tests_name = argv[argv.index('tests.json')]
+#f_values_name = argv[argv.index('values.json')]
+#f_report_name = argv[argv.index('report.json')]
+
+f_tests_name = os.path.abspath('tests.json')
+
+print(f_tests_name)
+print(os.getcwd())
+
+#with open(f_tests_name, "r") as f_tests:
+#    json.load(f_tests)
+
