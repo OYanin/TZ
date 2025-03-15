e919aa126fd4c60b1dbe09666116e523bf80b386
oyanin <[itcsid72@gmail.com]>
2025-03-15 11:35:47 +0500

    task4 complete

---
+++ task4/task4.py
@@ -0,0 +1,27 @@
+#from pathlib import Path
+from sys import argv
+
+try:
+    path = argv[1]
+except:
+    print('Проблемы с доступом к файлу')
+
+#script_dir = str(Path(__file__).parent)
+#path = script_dir + '/' + 'input.txt'
+
+with open(path, "r") as f_digits:
+    d_digits = [int(digit.replace('\n', '')) for digit in f_digits.readlines()]
+
+cnt = 0
+
+while not(all([sum(d_digits)/len(d_digits) == digit for digit in d_digits])):
+    cnt += 1
+    d_digits.sort()
+    average = sum(d_digits)/len(d_digits)
+
+    if average - d_digits[0] > d_digits[-1] - average:
+        d_digits[0] += 1
+    else:
+        d_digits[-1] -= 1
+
+print(cnt)
